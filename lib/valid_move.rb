ec7701c0dfb9d796c86426eabbfce645a3ec6cfb
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 20:13:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,7 +11,7 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board, index)
-  if position_taken? == " " || position_taken? == ""
+  if position_taken? 
     return true
   else
     return false
