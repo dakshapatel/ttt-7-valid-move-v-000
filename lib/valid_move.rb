e5f3e19f16591e9ac6c9b6fa4008a6ec7e2b1322
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 19:39:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,10 +10,9 @@ def position_taken?(board,index)
   end
 end
 
-def valid_move?(board,index)
-  if position_taken?
-    return true
-  else
-    return true
-  end
-end 
+def valid_move?(board, position)
+-  if position.to_i.between?(1,9)
+-    if !position_taken?(board, position.to_i-1)
+-      true
+-    end	
+-  end
