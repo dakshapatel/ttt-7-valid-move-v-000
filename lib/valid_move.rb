909d10dc6df0827ef522f09539f9dd721d82f813
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 19:40:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ end
 def valid_move?(board, position)
   if position.to_i.between?(1,9)
     return true
-    if !position_taken?(board, position.to_i-1)
+    if position_taken?(board, position.to_i-1)
     return true
     end
   end
